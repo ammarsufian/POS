43556d0f4fed87530c5fc1bf7f497919e73a665c
ammarsufian60 <ammarsufian60@gmail.com>
2022-12-24 17:41:18 +0300

    Apply the following changes:
    1- fix debit amount on the dashboard screen

--- app/Http/Controllers/ControlPageController.php
+++ app/Http/Controllers/ControlPageController.php
@@ -11,11 +11,10 @@ class ControlPageController extends Controller
 {
     public function index(Request $request)
     {
-        $ordersQuery = Order::where('created_at','>=',Carbon::now()->startOfDay())->withSum('items','total');
         return view('index')->with([
-            'today_sales' => $ordersQuery->get()->sum('items_sum_total'),
-            'today_cash_sales' => $ordersQuery->where('payment_method','cash')->get()->sum('items_sum_total'),
-            'today_debit_sales' => $ordersQuery->where('payment_method','debit')->get()->sum('items_sum_total'),
+            'today_sales' => Order::where('created_at','>=',Carbon::now()->startOfDay())->withSum('items','total')->get()->sum('items_sum_total'),
+            'today_cash_sales' => Order::where('created_at','>=',Carbon::now()->startOfDay())->withSum('items','total')->where('payment_method','cash')->get()->sum('items_sum_total'),
+            'today_debit_sales' => Order::where('created_at','>=',Carbon::now()->startOfDay())->withSum('items','total')->where('payment_method','debit')->get()->sum('items_sum_total'),
             'low_stock_products' => Product::where('quantity','<',10)->take(5)->get(),
         ]);
     }
