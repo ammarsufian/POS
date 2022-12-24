907fc4f2f273b5cfbaa34d2802bce8c3c05d455a
ammarsufian60 <ammarsufian60@gmail.com>
2022-12-24 23:27:46 +0300

    Apply the following changes:
    1- fix pos page

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -17,19 +17,22 @@ class PosController extends Controller
     {
         $cart = null;
         $customer = Cache::get('customer');
+        $debit_attributes = [];
 
         if ($customer) {
             $cart = Cart::where('customer_id', $customer->id)->first();
             $debit_data =CustomerTransaction::where('customer_id', $customer->id)->where('status', 'pending')->where('payment_type', 'debit');
+            $debit_attributes = [
+                'debit_amount' => $debit_data ? $debit_data->sum('total') :0,
+                'debits_history' => $debit_data->get() ?? null
+            ];
         }
 
-        return view('pos')->with([
+        return view('pos')->with(array_merge([
             'types' => CustomerType::get(),
             'active_customer' => $customer ?? null,
             'cart' => $cart ? (CartResource::make($cart))->resolve() : null,
             'customers_list' => Customer::whereHas('cart')->get(),
-            'debit_amount' => $debit_data->sum('total') ??0,
-            'debits_history' => $debit_data->get()
-        ]);
+        ],$debit_attributes ));
     }
 }
